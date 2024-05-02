edc6da7b4725799f3d4235170a6336f9de9477ec
TatianaHernandez4 <tatiana.hernandezposada@gmail.com>
2024-05-01 21:31:14 -0500

    Ejercicio1

---
+++ Ejercicio1/Ejercicio1.js
@@ -0,0 +1,12 @@
+//Escribe una función que tome dos números como parámetros y devuelva su suma.
+// Define la función para sumar dos números
+function sum(a, b) {
+    return a + b;
+}
+
+// Ejemplo de uso de la función
+const num1 = 3;
+const num2 = 5;
+const resultado = sum(num1, num2);
+
+console.log(`La suma de ${num1} y ${num2} es: ${resultado}`);
